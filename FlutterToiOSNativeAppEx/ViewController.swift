0bf08084fc9f94460d0cadde24d43cad42f86c7e
amisha <amisha.i@canopas.com>
2022-09-14 12:33:26 +0530

    Added iOS project with flutter view

--- FlutterToiOSNativeAppEx/ViewController.swift
+++ FlutterToiOSNativeAppEx/ViewController.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import Flutter
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onLoginBtnClick(_ sender: UIButton) {
+        let flutterEngine = (UIApplication.shared.delegate as! AppDelegate).flutterEngine
+        let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+        present(flutterViewController, animated: true, completion: nil)
+    }
 }
-
